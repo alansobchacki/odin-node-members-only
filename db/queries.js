82a57c0629f2105d799534d46c64b21e3d73b65c
Alan Sobchacki <a.sobchack@gmail.com>
2024-08-06 15:38:43 -0300

    Add models for handling messages.

--- db/queries.js
+++ db/queries.js
@@ -40,8 +40,39 @@ async function changeUserMembership(userName, membership = "member") {
   ]);
 }
 
+// all message related queries
+async function getAllMessages() {
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+}
+
+async function addNewMessage(userName, message) {
+  const userQuery = await pool.query(
+    "SELECT id FROM users WHERE username = $1",
+    [userName]
+  );
+
+  if (userQuery.rows.length === 0) {
+    throw new Error(`User "${userName}" not found.`);
+  }
+
+  const userId = userQuery.rows[0].id;
+
+  await pool.query("INSERT INTO messages (user_id, content) VALUES ($1, $2)", [
+    userId,
+    message,
+  ]);
+}
+
+async function deleteMessage(messageId) {
+  await pool.query("DELETE FROM messages WHERE id = $1", [messageId]);
+}
+
 module.exports = {
   getAllUsers,
   addNewUser,
   changeUserMembership,
+  getAllMessages,
+  addNewMessage,
+  deleteMessage,
 };
